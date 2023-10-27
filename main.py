2d3630b1b0774b5740913ae9a40c4eeb5f047c1a
John Doe <mdi.2005@mail.ru>
2023-10-27 21:27:15 +0500

    Co-authored-by: raest4ik <raest4ik@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-from re import X
 import tensorflow as tf
 from tensorflow import keras
 import os
 from typing import Literal
+from PIL import Image
 
 image_height = 360
 image_width = 640
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
+def splitset(arr):
+    kf = round(len(arr)*0.8)
+    trainarr = arr[0:kf]
+    testarr = arr[kf:]
+    return trainarr, testarr
+
 def load_dataset(pathnumber: Literal[0, 1, 2]):#startpath = 0|1|2
     path_ms = f"{current_dir}/dataset/video{pathnumber}/frames_ms"
     path_output = f"{current_dir}/dataset/video{pathnumber}/frames_output"
     images = os.listdir(path_ms)
     txts = os.listdir(path_output)
-    
-    return 
-    
+    txtarr = []
+    for txt in txts:
+        with open(f'{path_output}/{txt}', 'r', encoding="utf-8") as f:
+            txtarr.append([int(i.strip()) for i in f])
+    return splitset(images), splitset(txts)
 
-
-model = keras.models.Sequential([
+def mainlogic():
+    model = keras.models.Sequential([
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_height, image_width, 3)),
     keras.layers.MaxPooling2D((2, 2)),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
@@ -27,16 +35,32 @@ model = keras.models.Sequential([
     keras.layers.Flatten(),
     keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(2, activation='softmax')  # 2 classes: MSW and Healthy
-])
-
-model.compile(optimizer='adam',
+    ])
+    model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10, validation_data=(val_images, val_labels))
+    model.fit(train_images, train_labels, epochs=10, validation_data=(val_images, val_labels))
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print(f'Test accuracy: {test_acc}')
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print(f'Test accuracy: {test_acc}')
 
 
-predictions = model.predict(new_images)
+    predictions = model.predict(new_images)
+
+
+
+
+for i in 0,1,2:
+    [train_images,test_images], [train_labels,test_labels] = load_dataset(1)
+    
+    mainlogic(train_images, train_labels)
+
+    
+
+
+
+
+
+
+
